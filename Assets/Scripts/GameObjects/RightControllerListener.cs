722896026128837fad2a5c96a02595943b7eb9c6
Justin Hall <jhall.auburn@gmail.com>
2017-07-30 11:16:46 -0700

    added albums + floor collider

--- Assets/Scripts/GameObjects/RightControllerListener.cs
+++ Assets/Scripts/GameObjects/RightControllerListener.cs
@@ -4,13 +4,10 @@ using VRTK;
 using UnityEngine;
 
 public class RightControllerListener : MonoBehaviour {
- 
-	public GameObject records;
-	//public ShowKeyboard keyboardScript;
 
 	// Use this for initialization
 	void Start () {
-		// GetComponent<VRTK_ControllerEvents>().TriggerClicked += new ControllerInteractionEventHandler(DoTriggerClicked);
+		GetComponent<VRTK_ControllerEvents>().TriggerClicked += new ControllerInteractionEventHandler(DoTriggerClicked);
 		// GetComponent<VRTK_ControllerEvents> ().TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadPressed);
 	}
 
@@ -18,12 +15,8 @@ public class RightControllerListener : MonoBehaviour {
 		VRTK_Logger.Info("Controller on index '" + index + "' " + button + " has been " + action
 			+ " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
 	}
-	/*
+
 	private void DoTriggerClicked(object sender, ControllerInteractionEventArgs e) {
-		if (!records.activeInHierarchy) {
-			records.SetActive (true);
-			VRTK_ControllerHaptics.TriggerHapticPulse(VRTK_ControllerReference.GetControllerReference(GetComponent<VRTK_ControllerEvents>().gameObject), 0.33f);//, 0.1f, 0.01f);
-		}
 		DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "clicked", e);
 	}
 
@@ -31,5 +24,5 @@ public class RightControllerListener : MonoBehaviour {
 	private void DoTouchpadPressed(object sender, ControllerInteractionEventArgs e) {
 		
 	}
-	*/
+
 }
