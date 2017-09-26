da82d6fb40d805577d2c0e985b74f9e4865c4e3a
Justin Hall <jhall.auburn@gmail.com>
2017-09-25 21:39:45 -0700

    added prototype scene

---
+++ Assets/Scripts/_SceneSelector/Balloon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Balloon : VRTK_InteractableObject {
+
+	public float balloonSpeed;
+
+	private bool isUsing;
+
+	protected override void Update ()
+	{
+		base.Update ();
+		Debug.Log ("isUsing?: " + isUsing);
+		if (isUsing) {
+			gameObject.transform.Translate (0.0f, balloonSpeed * Time.deltaTime, 0.0f);
+		}
+	}
+
+	public override void StartUsing (VRTK_InteractUse currentUsingObject)
+	{
+		base.StartUsing (currentUsingObject);
+		gameObject.GetComponent<MeshRenderer> ().material.color = Color.red;
+		isUsing = true;
+	}
+
+	public override void StopUsing (VRTK_InteractUse previousUsingObject)
+	{
+		base.StopUsing (previousUsingObject);
+		isUsing = false;
+	}
+		
+}
