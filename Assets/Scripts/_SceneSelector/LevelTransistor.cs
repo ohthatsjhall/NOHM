56fa77fc7ce227cce9354ce31c740e041b175843
Justin Hall <jhall.auburn@gmail.com>
2017-09-30 16:47:12 -0700

    added trigger collider for scene change

---
+++ Assets/Scripts/_SceneSelector/LevelTransistor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransistor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("game object " + collider.gameObject);
+		Debug.Log ("hit");
+	}
+}
