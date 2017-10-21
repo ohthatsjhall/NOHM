853a366d7129a6602e7f3fadfc160a5ee31d220f
Justin Hall <jhall.auburn@gmail.com>
2017-10-21 16:27:26 -0700

    added editor extension for clearing prefs

---
+++ Assets/Editor/LevelTransistionEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Balloon))]
+public class LevelTransistionEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector ();
+		if (GUILayout.Button ("Clear Player Prefs"))
+			PlayerPrefs.DeleteKey ("OnboardingCompleted");
+	}
+}
